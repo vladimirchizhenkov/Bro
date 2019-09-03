3f52bdc27f5cfc36f8f31c3daf5c3ff0d116a583
w.chizhenkov <w.chizhenkov@gmail.com>
2019-09-04 03:57:49 +0400

    DLTEST || slider complete

--- app/js/common-jq.js
+++ app/js/common-jq.js
@@ -5,5 +5,9 @@ $('.testimonials__slider').slick({
     dots: true,
     arrows: true,
     prevArrow: $('.testimonials__arrow-left'),
-    nextArrow: $('.testimonials__arrow-right')
+    nextArrow: $('.testimonials__arrow-right'),
+    customPaging : function(slider, i) {
+        return '<div class="testimonials__dots"></div>';
+    }
 });
+
