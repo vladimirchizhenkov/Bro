2d64b3417ea9128fd6fda8df5d6b827b096f5cce
w.chizhenkov <w.chizhenkov@gmail.com>
2019-09-08 16:21:44 +0400

    DLTEST || correct mobile size block of contacts

--- app/js/common-jq.js
+++ app/js/common-jq.js
@@ -6,8 +6,21 @@ $('.testimonials__slider').slick({
     arrows: true,
     prevArrow: $('.testimonials__arrow-left'),
     nextArrow: $('.testimonials__arrow-right'),
-    customPaging : function(slider, i) {
+    customPaging: function (slider, i) {
         return '<div class="testimonials__dots"></div>';
-    }
+    },
+    responsive: [
+        {
+            breakpoint: 320,
+            settings: {
+                mobileFirst: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: true,
+                arrows: false
+            }
+        }
+    ]
 });
 
