76b95f4e47ed542d250274c7f0926dc9f4f2f615
w.chizhenkov <w.chizhenkov@gmail.com>
2019-09-09 02:34:26 +0400

    DLTEST || correct contact style

--- app/js/common-jq.js
+++ app/js/common-jq.js
@@ -10,6 +10,21 @@ $('.testimonials__slider').slick({
     arrows: false,
     responsive: [
         {
+            breakpoint: 768,
+            settings: {
+                infinite: false,
+                slidesToScroll: 2,
+                slidesToShow: 2,
+                dots: true,
+                arrows: true,
+                prevArrow: $('.testimonials__arrow-left'),
+                nextArrow: $('.testimonials__arrow-right'),
+                customPaging: function (slider, i) {
+                    return '<div class="testimonials__dots"></div>';
+                }
+            }
+        },
+        {
             breakpoint: 1180,
             settings: {
                 infinite: false,
