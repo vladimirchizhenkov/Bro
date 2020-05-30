2a56ff0132e92240d047095fa7b8f0f5fe4e3b98
Vladimir Chizhenkov <w.chizhenkov@gmail.com>
2020-05-31 01:11:56 +0400

    slider foo complete

--- app/js/common-jq.js
+++ app/js/common-jq.js
@@ -1,33 +1,135 @@
 $(document).ready(function() {
+    var btnPrev = $('#btnPrev');
+    var btnNext = $('#btnNext');
+    var tabs = $('.slider__tab');
+    var slides = $('.slider__item');
 
-    var tab = $('#myTab');
-    var num = tab.find('.nav-tabs li').length - 1;
+    // Инициализируем слайдер
+    sliderInit();
 
-    $('.next').click(function(){
-        var cur = tab.find('li.active');
-        var index = cur.index();
+    // Клик в таб
+    tabs.on('click', function() {
+        // Проверяем статус стрелок
+        checkArrowStatus();
 
-        if(index == num) {
-            var next = tab.find('.nav-tabs').find('li').eq(0);
-        }
-        else {
-            var next = cur.next('li');
-        }
+        // Добавляем ко всем табам класс slider__tab--default
+        tabs.each(function() {
+           $(this).addClass('slider__tab--default');
+        });
 
-        next.find('a').tab('show');
+        // Удаляем default-класс у выбранного элемента
+        $(this).removeClass('slider__tab--default');
+
+        // Получаем ID текущего таба
+        var currentID = $(this).attr('id');
+        currentID = `'` + currentID + `'`
+
+        // Получаем по текущему ID соответствующий слайд по data-project аттрибуту
+        var currentSlide = $(".slider__item[data-project=" + currentID +"]");
+
+        // Добавляем ко всем слайдам класс hide
+        slides.each(function() {
+           $(this).addClass('slider__item--hide');
+        });
+
+        // Удаляем для текущего слайда статус hide
+        currentSlide.removeClass('slider__item--hide');
+
+        // Проверяем статус стрелок
+        checkArrowStatus();
     });
 
-    $('.prev').click(function(){
-        var cur = tab.find('li.active');
-        var index = cur.index();
+    // Клик в стрелки
+    // Проверяем какой таб сейчас выбран
+    btnPrev.on('click', function() {
+        // Получаем индекс текущего слайда
+        // Находим текущий слайд и его индекс в массиве
+        var currentProject = slides.not('.slider__item--hide').attr('data-project');
+        var currentSlideIndex = slides.not('.slider__item--hide').index();
 
-        if(index == 0) {
-            var next = tab.find('.nav-tabs').find('li').eq(num);
-        }
-        else {
-            var next = cur.prev('li');
+        if (currentSlideIndex === 0) {
+            return false;
         }
 
-        next.find('a').tab('show');
+        // Получаем предыдущий слайд
+        var prevSlide = $('.slider__item').not('.slider__item--hide').prev();
+
+        // Скрываем все слайды
+        slides.each(function () {
+            $(this).addClass('slider__item--hide');
+        })
+
+        // Показываем предыдущий слайд
+        prevSlide.removeClass('slider__item--hide');
+
+        // Переключаем табы
+        tabs.each(function () {
+            $(this).addClass('slider__tab--default')
+        });
+
+        $('#' + currentProject).prev().removeClass('slider__tab--default');
+
+        // Проверяем статус стрелок
+        checkArrowStatus();
     });
+
+    btnNext.on('click', function() {
+        // Получаем имя проекта из дата-аттрибута
+        var currentProject = slides.not('.slider__item--hide').attr('data-project');
+
+        // Находим текущий слайд и его индекс в массиве
+        var currentSlideIndex = slides.not('.slider__item--hide').index();
+        var lastSlide = tabs.length - 1;
+
+        if (currentSlideIndex === lastSlide) {
+            return false;
+        }
+
+        // Получаем следующий слайд
+        var nextSlide = slides.not('.slider__item--hide').next();
+
+        // Скрываем все слайды
+        slides.each(function () {
+            $(this).addClass('slider__item--hide');
+        })
+
+        // Показываем следующий слайд
+        nextSlide.removeClass('slider__item--hide');
+
+        // Переключаем табы:
+        // Добавляем дефолтные свойства
+        tabs.each(function () {
+            $(this).addClass('slider__tab--default')
+        });
+
+        $('#' + currentProject).next().removeClass('slider__tab--default');
+
+        // Проверяем статус стрелок
+        checkArrowStatus();
+    });
+
+    // Функция инициализации слайдера
+    function sliderInit() {
+        checkArrowStatus();
+    }
+
+    // Функция проверки статуса стрелок
+    function checkArrowStatus() {
+        var currentSlideIndex = slides.not('.slider__item--hide').index();
+        var lastSlide = tabs.length - 1;
+
+        if (currentSlideIndex === 0) {
+            btnPrev.addClass('slider__arrow--disable');
+        } else {
+            btnPrev.removeClass('slider__arrow--disable');
+        }
+
+        if (currentSlideIndex === lastSlide) {
+            btnNext.addClass('slider__arrow--disable');
+        } else {
+            btnNext.removeClass('slider__arrow--disable');
+        }
+
+    }
+
 });
